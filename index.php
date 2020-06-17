b8e04b58741de175eeb850afd7eec0eb99131987
Task Nagashige <task.19.xx@gmail.com>
2020-06-17 11:53:23 +0900

    デザインを調整した

--- index.php
+++ index.php
@@ -1,23 +1,30 @@
 <html>
-<head><title>掲示板App</title></head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>掲示板App</title>
+</head>
 <body>
 
-<h1>掲示板</h1>
-
-<h2>投稿フォーム</h2>
+<div class="container">
+    <div class="col-md-8">
+        <h1 class="text-center text-primary py-3">掲示板App</h1>
+        
+        <h2 class="text-muted py-3">投稿フォーム</h2>
 
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-    <input type="text" name="personal_name" placeholder="名前" required><br><br>
-    <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
+    <input type="text" class="form-control" name="personal_name" placeholder="名前" required><br><br>
+    <textarea name="contents" class="form-control" rows="8" cols="40" placeholder="内容" required>
 </textarea><br><br>
-    <input type="submit" name="btn" value="投稿する">
+    <input class="btn btn-primary"  type="submit" name="btn" value="投稿する">
 </form>
 
-<h2>スレッド</h2>
+<h2 class="text-muted py-3">スレッド</h2>
 
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
     <input type="hidden" name="method" value="DELETE">
-    <button type="submit">投稿を全削除する</button>
+    <button class="btn btn-danger" type="submit">投稿を全削除する</button>
 </form>
 
 <?php
@@ -87,5 +94,12 @@ readData();
 
 ?>
 
+    </div>
+</div>
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
