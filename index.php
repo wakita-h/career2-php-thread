f673b8a10f37912bd7d5aba5944a1d6d1ba14d92
Task Nagashige <task.19.xx@gmail.com>
2020-06-29 00:55:42 +0900

    PSR2に沿ってコードを修正

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
+<?php
+/**
+ * 職業実践2 - 掲示板アプリ
+ */
+?>
+
 <html lang="ja">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" rel="stylesheet">
     <title>掲示板App</title>
 </head>
 <body>
@@ -31,7 +38,8 @@
 date_default_timezone_set('Asia/Tokyo');
 const THREAD_FILE = 'thread.txt';
 
-function readData() {
+function readData()
+{
     // ファイルが存在しなければデフォルト空文字のファイルを作成する
     if (! file_exists(THREAD_FILE)) {
         $fp = fopen(THREAD_FILE, 'w');
@@ -43,7 +51,8 @@ function readData() {
     echo $thread_text;
 }
 
-function writeData() {
+function writeData()
+{
     $personal_name = $_POST['personal_name'];
     $contents = $_POST['contents'];
     $contents = nl2br($contents);
@@ -57,14 +66,14 @@ function writeData() {
     $fp = fopen(THREAD_FILE, 'a');
     
 
-    if ($fp){
-        if (flock($fp, LOCK_EX)){
-            if (fwrite($fp,  $data) === FALSE){
+    if ($fp) {
+        if (flock($fp, LOCK_EX)) {
+            if (fwrite($fp, $data) === false) {
                 print('ファイル書き込みに失敗しました');
             }
 
             flock($fp, LOCK_UN);
-        }else{
+        } else {
             print('ファイルロックに失敗しました');
         }
     }
@@ -72,7 +81,8 @@ function writeData() {
     fclose($fp);
 }
 
-function deleteData() {
+function deleteData()
+{
     file_put_contents(THREAD_FILE, "");
 }
 
@@ -97,8 +107,14 @@ readData();
 </div>
 
 <!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+</script>
 </body>
 </html>
