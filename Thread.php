f3a9aa2639f33f59150d1207336fcfec89a00f80
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-07-15 09:18:55 +0900

    thread

--- Thread.php
+++ Thread.php
@@ -1,34 +1,54 @@
 <?php
 
-class Thread {
+date_default_timezone_set('Asia/Tokyo');
 
+class Thread
+{
     private $name;
     private const THREAD_FILE = 'thread.txt';
+    private $dbh;
 
     public function __construct(string $name)
     {
         $this->name = $name;
+        $this->dbh = new PDO('mysql:dbname=thread;host=127.0.0.1', 'root', 'password');
     }
-    
+
     public function getList()
     {
-        return file_get_contents(self::THREAD_FILE);
-    }
-    
-    public function post(string $personal_name, string $contents) 
-    {
-        $data = "<hr>\n";
-        $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
-        $data = $data."<p>投稿者:".$personal_name."</p>\n";
-        $data = $data."<p>内容:</p>\n";
-        $data = $data."<p>".$contents."</p>\n";
+        $res = "";
+        $sql = "SELECT * FROM `thread` WHERE `deleted_at` IS NULL ORDER BY `created_at` DESC";
+        $stmt = $this->dbh->query($sql);
+        while($row = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+            $created_at = $row["created_at"];
+            $name = $row["name"];
+            $contents = $row["content"];
+            $res .= "<hr>\n";
+            $res .= "<p>投稿日時: ".date("Y/m/d H:i:s", strtotime($created_at))."</p>\n";
+            $res .= "<p>投稿者:".$name."</p>\n";
+            $res .= "<p>内容:</p>\n";
+            $res .= "<p>".nl2br($contents)."</p>\n";
+        }
 
-        file_put_contents(self::THREAD_FILE, $data, FILE_APPEND);
+        return $res;
     }
 
-    public function delete()
+    /**
+     * @param string $personal_name
+     * @param string $contents
+     */
+    public function post(string $personal_name, string $contents)
     {
-        file_put_contents(self::THREAD_FILE, "");
+        $sql ="INSERT INTO `thread` (name, content) VALUE (:name, :content)";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->bindParam(':name', $personal_name, PDO::PARAM_STR);
+        $stmt->bindParam(':content', $contents, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+
+    public function delete() {
+        $sql = "UPDATE `thread` SET `deleted_at` = NOW()";
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute();
     }
 }
-?>
